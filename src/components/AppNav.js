a029926f15f8358fbe55c749261663befd34fa7d
Arif Faysal <ayon.arif.10@gmail.com>
2020-12-28 20:48:21 +0600

    navBar & homePage

--- src/components/AppNav.js
+++ src/components/AppNav.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import { SocialIcon } from 'react-social-icons';
 
 class AppNav extends Component {
     render() {
@@ -14,24 +15,35 @@ class AppNav extends Component {
                         hover:text-green-800 
                         text-4xl font-bold 
                         cursive tracking-widest"
-                        activeClassName="text-white"
+                        activeClassName="text-red-100 bg-red-700"
                         >React & Tailwindv2.0</NavLink>
+
                         <NavLink to="/post"
                         className="inline-flex items-center 
                         py-3 px-3 my-6 
                         rounded 
-                        text-red-200 hover:text-green-800">Blog Posts</NavLink>
+                        text-red-200 hover:text-green-800"
+                        activeClassName="text-red-100 bg-red-700">Blog Posts</NavLink>
+                        
                         <NavLink to="/project"
                         className="inline-flex items-center 
                         py-3 px-3 my-6 
                         rounded 
-                        text-red-200 hover:text-green-800">Project</NavLink>
+                        text-red-200 hover:text-green-800"
+                        activeClassName="text-red-100 bg-red-700">Project</NavLink>
+                        
                         <NavLink to="/about"
                         className="inline-flex items-center 
                         py-3 px-3 my-6 
                         rounded 
-                        text-red-200 hover:text-green-800">About Me</NavLink>
+                        text-red-200 hover:text-green-800"
+                        activeClassName="text-red-100 bg-red-700">About Me</NavLink>
                     </nav>
+                    <div className="inline-flex py-3 px-3 my-6">
+                        <SocialIcon className="mr-4" fgColor="#fff" target="_blank" url="http://linkedin.com/in/arif-faysal-17094b130" style={{ height:35, width:35 }}/>
+                        <SocialIcon className="mr-4" fgColor="#fff" target="_blank" url="http://facebook.com/arif.f.ayon" style={{ height:35, width:35 }}/>
+                        <SocialIcon className="mr-4" fgColor="#fff" target="_blank" url="http://github.com/BlackKnightZERO" style={{ height:35, width:35 }}/>
+                    </div>
                 </div>
             </header>
         );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,9 +3,14 @@ import React, { Component } from 'react';
 class Home extends Component {
     render() {
         return (
-            <div>
-                <h1>Home</h1>
-            </div>
+            <main>
+                <img src="./img/bkz.jpg" className="absolute object-cover w-full h-full" />
+                <section className="relative flex justify-center min-h-screen pt-12 lg:pt-80 px-4">
+                    <h1 className="text-6xl text-green-100 font-bold 
+                    cursive leading-none 
+                    lg:leading-snug home-name">Hi, I'm Z3R0</h1>
+                </section>
+            </main>
         );
     }
 }
